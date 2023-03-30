bc47494e7fdf80607084f3d0fcd9d10ae7dc39c1
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-03-30 12:19:09 -0400

    function to delete and update

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -65,10 +65,43 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+        id: req.params.id
+    }
+  })
+    .then(dbCategories => {
+        if (!dbCategories[0]) {
+            res.status(404).json({ message: 'No category found with this id'});
+            return;
+        }
+        res.json(dbCategories);
+  })
+    .catch(err => {
+        console.log(err); 
+        res.status(500).json(err);
+  });
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.remove({
+    where: {
+        id: req.params.id
+    }
+  })
+    .then(dbCategories => {
+        if (!dbCategories) {
+            res.status(404).json({ message: 'Error: No User Found'});
+            return;
+        }
+        res.json(dbCategories);
+  })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+  });
 });
 
 module.exports = router;
